89685814fca3b2427f32eb234b6e47aa751eed99
Tarik <tarikozgur0607@gmail.com>
2023-04-28 15:12:11 +0300

    add new codes

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+let bookArray =["Hobbit","Harry Potter","Silmarillion"];
+
+// function Book(author,title,number,read){
+//     this.author = author;
+//     this.title = title;
+//     this.number = number;
+//     this.read = read;
  
+// }
+
+function addToLibrary(){
+    const button = document.getElementById(".button");
+    button.addEventListener(click,()=>{
+        alert("?")
+    })
+    document.appendChild(button)
+
+}
+addToLibrary()
+
+for (let i = 0; i < bookArray.length; i++) {
+    const element = bookArray[i];
+    console.log(element)
+ }
