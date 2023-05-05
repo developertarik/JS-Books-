0b85ce635a5c444d3574d79ed17483a9d85382f9
Tarik <tarikozgur0607@gmail.com>
2023-05-05 15:57:01 +0300

    add new codes

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 let bookArray =["Hobbit","Harry Potter","Silmarillion"];
 
-// function Book(author,title,number,read){
-//     this.author = author;
-//     this.title = title;
-//     this.number = number;
-//     this.read = read;
+  function Book(author,title,number,read){
+      this.author = author;
+      this.title = title;
+     this.number = number;
+     this.read = read;
  
+  }
+  const button = document.getElementById(".button");
+// function addToLibrary(author,title,number,read){
+    
+//     button.addEventListener(click,()=>{
+//        author = prompt("How is author?")
+//        title = prompt("how is title")
+//        number = prompt("how many pages?")
+//        read = prompt("read or not read")
+//     })
+//     document.appendChild(button)
+
 // }
-
-function addToLibrary(){
-    const button = document.getElementById(".button");
-    button.addEventListener(click,()=>{
-        alert("?")
-    })
-    document.appendChild(button)
-
-}
-addToLibrary()
-
-for (let i = 0; i < bookArray.length; i++) {
-    const element = bookArray[i];
-    console.log(element)
- }
+// addToLibrary()
+let analog  = document.getElementById("analog")
+for (let i = 0; i < bookArray.length; ++i) {
+    let td  = document.createElement("td");
+    td.innerText = bookArray[i];
+     analog.appendChild(td)
+    }
+    // for (i = 0; i < data.length; ++i) {
+    //     var li = document.createElement('li');
+    //     li.innerText = data[i];
+    //     list.appendChild(li);
