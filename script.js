1d25b1f2bb92635b248c848f4cd5d311a8f2405a
Tarik <tarikozgur0607@gmail.com>
2023-06-17 14:15:59 +0300

    new codes :)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let bookArray =["Silmarillion","BİİİPS","didiiiiiiiit"];
+let bookArray =[];
 
   function Book(author,title,pages,read){
     this.author = author
@@ -10,89 +10,38 @@ let bookArray =["Silmarillion","BİİİPS","didiiiiiiiit"];
 
   
 Book();
-analog  = document.getElementById("analog")
-for (let i = 0; i < bookArray.length; ++i) {
-      let td  = document.createElement("td");
-      td.innerText = bookArray[i];
+//buttons
+const button = document.getElementById("button")
+const submit = document.getElementById("submit")
+const title = document.getElementById("title")
+function viewToLibrary(){
+  for (let i = 0; i< bookArray.length; i++) {
+    let td = document.createElement("td");
+    td.innerText = bookArray[i];
+    analog.appendChild(td)
+}
 
-       analog.appendChild(td)
-     }
- 
+
+}
+viewToLibrary()
+//add to library
 function addToLibrary(){
-  const form  = document.getElementById("form")
-  const button = document.getElementById("button")
-  const submit = document.getElementById("submit")
-   title = document.getElementById("title")
+ titleValue = document.getElementById("title").value
+ bookArray.push(titleValue)
+ console.log(titleValue)
+ viewToLibrary()
+
+
+}
+
+ 
 
   button.addEventListener("click",()=>{
     form.style.visibility = "visible";
   })
-}
   
-  submit.addEventListener("click",()=>{
-  bookArray += title.innerText
-  })
-  
-  
-  
-  
-  addToLibrary()
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  //variables
 
-
-// const button = document.getElementById("button");
-// const form = document.getElementById("form")
-// const click_button =  document.createElement("button")
-// function visible(){
-//   form.style.visibility = "visible";
-// }
-// function addToLibrary(visible){
-
-//       button.addEventListener("click",visible)
-//   };    
-
-// // }
-//  addToLibrary(visible)
-//   let analog  = document.getElementById("analog")
-//     for (let i = 0; i < bookArray.length; ++i) {
-//         let td  = document.createElement("td");
-//        td.innerText = bookArray[i];
-   
-//          analog.appendChild(td)
-//        }
-    
-// // let submit = document.getElementById("submit");
-// // function visibleBooks(){
-// //   let td  = document.createElement("td");
-// //   const i = analog.innerText
-// //   td.innerText = bookArray[i];
   
-// //    analog.appendChild(td)
-// // };
-// // submit.addEventListener("click",visibleBooks);
-
-
-// function anew() {
-//   let x = document.getElementById("form");
-//   let text = "";
-//   let i;
-//   for (i = 0; i < x.length ;i++) {
-//     text += x.elements[i].value + "<br>";
-//   }
-//   document.getElementById("form").innerHTML = text;}
-
-// submit.addEventListener("click",anew)
-// 
+  
+  
+submit.addEventListener("click",addToLibrary)
