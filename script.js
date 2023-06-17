4e64200b9c937332d7842b1c67b83df57ac52b30
Tarik <tarikozgur0607@gmail.com>
2023-06-17 18:55:42 +0300

    add new codes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let bookArray =[];
+let bookArray =["hobbit 399 read","silmarillion 299 read","create 0 0 read"];
 
   function Book(author,title,pages,read){
     this.author = author
@@ -9,39 +9,34 @@ let bookArray =[];
   }
 
   
-Book();
+ Book();
 //buttons
 const button = document.getElementById("button")
 const submit = document.getElementById("submit")
-const title = document.getElementById("title")
-function viewToLibrary(){
-  for (let i = 0; i< bookArray.length; i++) {
-    let td = document.createElement("td");
-    td.innerText = bookArray[i];
-    analog.appendChild(td)
-}
+const table = document.getElementById("table")
+// // function viewToLibrary(){
+// //   for (let i = 0; i< bookArray.length; i++) {
+// //     let td = document.createElement("td");
+// //     td.innerText = bookArray[i];
+// //     table.appendChild(td)
+// // }
 
 
-}
-viewToLibrary()
+// }
 //add to library
 function addToLibrary(){
- titleValue = document.getElementById("title").value
- bookArray.push(titleValue)
- console.log(titleValue)
- viewToLibrary()
-
-
+  author = document.getElementById("author").value
+  title  = document.getElementById("title").value
+  pages = document.getElementById("pages").value
+  read = document.getElementById("read").value
+  console.log(author,title,pages,read)
 }
 
+addToLibrary()
  
 
-  button.addEventListener("click",()=>{
-    form.style.visibility = "visible";
-  })
   
 
   
   
   
-submit.addEventListener("click",addToLibrary)
