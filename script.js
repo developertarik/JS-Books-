0bdec743bbf7d6afe1340135857878070d67bfa8
Tarik <tarikozgur0607@gmail.com>
2023-06-25 11:35:47 +0300

    add clear code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let bookArray =["hobbit 399 read","silmarillion 299 read","create 0 0 read"];
-
+// let bookArray = [Book]
   function Book(author,title,pages,read){
     this.author = author
     this.title = title
@@ -7,67 +6,69 @@ let bookArray =["hobbit 399 read","silmarillion 299 read","create 0 0 read"];
     this.read = read
    
   }
-
-  
+const newBook= undefined;
+let newBook2;
 //buttons
 const button = document.getElementById("button")
 const submit = document.getElementById("submit")
 const div = document.getElementById("container")
- const checkbox = document.getElementById("checkbox")
+const readBtn = document.createElement("input")
+readBtn
+console.log(newBook)
+
 
 // }
 //add to library
+const form  = document.getElementById("form")
+
 button.addEventListener("click",()=>{
   form.style.visibility = "visible";
 })
+//delete
 
-function addToLibrary(){
-  author = document.getElementById("author").value
-  title  = document.getElementById("title").value
-  pages = document.getElementById("pages").value
-  const div2 = document.createElement("div");
-
-  
+function addToLibrary( ){
+  const div2 = document.createElement("div")
   const deleteButton = document.createElement("button");
-  const readButton= document.createElement("button");
-  deleteButton.addEventListener("click",()=>{
-  deleteButton.innerText = "Button is delete";
+  readBtn.addEventListener("click",()=>{
+    newBook2.read = "not read!"
+  })
+  div2.innerHTML += newBook2.author + newBook2.title + newBook2.pages + newBook2.read;
+
+
+ deleteButton.setAttribute('style', 'width: 50px; height: 25px;');
+ deleteButton.addEventListener("click",()=>{
   div2.remove();
   deleteButton.remove();
-  })
-  if(checkbox.checked){
-    div2.innerHTML +="read";
-}
-else{
-  div2.innerHTML +="not read!";
-}
-  div2.innerHTML += [author,title,pages]
-  let text = document.createTextNode("Delete");
+})
+let text = document.createTextNode("Delete");
    deleteButton.appendChild(text);
-   div2.appendChild(readButton)
-  deleteButton.setAttribute('style', 'width: 50px; height: 25px;');
-  
   div2.setAttribute("id","box")
+  div2.appendChild(readBtn)
   div2.appendChild(deleteButton)
   
 
   div.appendChild(div2)
-}
- const form  = document.getElementById("form")
+ }
+//delete
+
 form.addEventListener("submit",(e)=>{
   e.preventDefault();
- 
-  const bookArray2 = [author,title,pages]
-  bookArray = bookArray2
-  console.log(bookArray)
+  author = document.getElementById("author").value
+  title = document.getElementById("title").value
+  pages = document.getElementById("pages").value
+  read = document.getElementById("read").value
+  newBook2 = new Book(author,title,pages,read)
+  console.log(newBook2.author)
   form.style.visibility = "hidden";
   addToLibrary();
-  author = document.getElementById("author").value = "";
-  title  = document.getElementById("title").value = " ";
-  pages = document.getElementById("pages").value = " ";
-
+ 
+  author = document.getElementById("author").value = ""
+  title = document.getElementById("title").value = ""
+  pages = document.getElementById("pages").value = ""
+  read = document.getElementById("read").value = ""
 
  }); 
 
   
+
   
